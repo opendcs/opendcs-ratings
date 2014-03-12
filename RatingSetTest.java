d3da965a5c37b0eb7e8b9d7d173d69783f23762d
Andreas Christman <andreas@rmanet.com>
2014-03-12 09:51:13 -0800

    added test case for depth table
    [git-p4: depot-paths = "//hecjavadev/dev/test/hec/data/cwmsRating/": change = 55497]

--- RatingSetTest.java
+++ RatingSetTest.java
@@ -59,6 +59,7 @@ import static hec.data.Parameter.PARAMID_TRAVEL;
 import static hec.data.Parameter.PARAMID_VOLUME;
 import static hec.data.Parameter.PARAMID_WIDTH;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -133,34 +134,98 @@ public class RatingSetTest
 	      "</point>"+
 	      	    "</rating-points>"+
 	  "</rating>"+
+	"</ratings>";
+	String _xmlText1 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
+	"<ratings xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.hec.usace.army.mil/xmlSchema/cwms/Ratings.xsd\">"+
+	  "<rating-template office-id=\"SWT\">"+
+	    "<parameters-id>%-DO-Saturation,%-DO-Saturation,%-DO-Saturation,%-DO-Saturation;%-DO-Saturation</parameters-id>"+
+	    "<version>Standard</version>"+
+	    "<ind-parameter-specs>"+
+	      "<ind-parameter-spec position=\"1\">"+
+	        "<parameter>%-DO-Saturation</parameter>"+
+	        "<in-range-method>ERROR</in-range-method>"+
+	       "<out-range-low-method>ERROR</out-range-low-method>"+
+	        "<out-range-high-method>NULL</out-range-high-method>"+
+	      "</ind-parameter-spec>"+
+	      "<ind-parameter-spec position=\"2\">"+
+	        "<parameter>%-DO-Saturation</parameter>"+
+	       "<in-range-method>NULL</in-range-method>"+
+	       "<out-range-low-method>NULL</out-range-low-method>"+
+	        "<out-range-high-method>NULL</out-range-high-method>"+
+	     " </ind-parameter-spec>"+
+	      "<ind-parameter-spec position=\"3\">"+
+	        "<parameter>%-DO-Saturation</parameter>"+
+	        "<in-range-method>NULL</in-range-method>"+
+	        "<out-range-low-method>NULL</out-range-low-method>"+
+	        "<out-range-high-method>NULL</out-range-high-method>"+
+	      "</ind-parameter-spec>"+
+	      "<ind-parameter-spec position=\"4\">"+
+	        "<parameter>%-DO-Saturation</parameter>"+
+	        "<in-range-method>NULL</in-range-method>"+
+	        "<out-range-low-method>NULL</out-range-low-method>"+
+	        "<out-range-high-method>NULL</out-range-high-method>"+
+	      "</ind-parameter-spec>"+
+	    "</ind-parameter-specs>"+
+	    "<dep-parameter>%-DO-Saturation</dep-parameter>"+
+	    "<description>22</description>"+
+	 " </rating-template>"+
+	  "<rating-spec office-id=\"SWT\">"+
+	    "<rating-spec-id>ALTU.%-DO-Saturation,%-DO-Saturation,%-DO-Saturation,%-DO-Saturation;%-DO-Saturation.Standard.Production1</rating-spec-id>"+
+	    "<template-id>%-DO-Saturation,%-DO-Saturation,%-DO-Saturation,%-DO-Saturation;%-DO-Saturation.Standard</template-id>"+
+	    "<location-id>ALTU</location-id>"+
+	    "<version>Production1</version>"+
+	    "<source-agency/>"+
+	    "<in-range-method>LINEAR</in-range-method>"+
+	    "<out-range-low-method>NULL</out-range-low-method>"+
+	    "<out-range-high-method>LINEAR</out-range-high-method>"+
+	    "<active>false</active>"+
+	    "<auto-update>false</auto-update>"+
+	    "<auto-activate>false</auto-activate>"+
+	    "<auto-migrate-extension>false</auto-migrate-extension>"+
+	    "<ind-rounding-specs>"+
+	      "<ind-rounding-spec position=\"1\">2222233332</ind-rounding-spec>"+
+	      "<ind-rounding-spec position=\"2\">2222233332</ind-rounding-spec>"+
+	      "<ind-rounding-spec position=\"3\">2222233332</ind-rounding-spec>"+
+	      "<ind-rounding-spec position=\"4\">2222233332</ind-rounding-spec>"+
+	    "</ind-rounding-specs>"+
+	    "<dep-rounding-spec>2222233332</dep-rounding-spec>"+
+	    "<description>1111</description>"+
+	 " </rating-spec>"+
 	  "<rating office-id=\"SWT\">"+
-	    "<rating-spec-id>ELDR.Elev;Area.Linear.Production</rating-spec-id>"+
-	    "<vertical-datum-info unit=\"ft\">"+
-	      "<native-datum>NAVD-88</native-datum>"+
-	      "<elevation>1271</elevation>"+
-	    "</vertical-datum-info>"+
-	    "<units-id>ft;acre</units-id>"+
-	    "<effective-date>2004-01-01T00:00:00-06:00</effective-date>"+
-	    "<create-date>2012-11-03T12:26:16-05:00</create-date>"+
+	    "<rating-spec-id>ALTU.%-DO-Saturation,%-DO-Saturation,%-DO-Saturation,%-DO-Saturation;%-DO-Saturation.Standard.Production1</rating-spec-id>"+
+	    "<units-id>%,%,%,%;%</units-id>"+
+	    "<effective-date>2014-03-11T18:13:00-05:00</effective-date>"+
+	    "<create-date>2014-03-11T18:13:00-05:00</create-date>"+
 	    "<active>true</active>"+
-	    "<description/>"+
+	    "<description>1111</description>"+
 	    "<rating-points>"+
+	      "<other-ind position=\"1\" value=\"0\"/>"+
+	      "<other-ind position=\"2\" value=\"0\"/>"+
+	      "<other-ind position=\"3\" value=\"0\"/>"+
 	      "<point>"+
-	        "<ind>1279</ind>"+
+	        "<ind>0</ind>"+
 	        "<dep>0</dep>"+
 	      "</point>"+
 	      "<point>"+
-	        "<ind>1280</ind>"+
-	        "<dep>0</dep>"+
-	      "</point>"+
-	     
-	  "  </rating-points>"+
+	        "<ind>1</ind>"+
+	        "<dep>2</dep>"+
+	     " </point>"+
+	    "</rating-points>"+
 	  "</rating>"+
 	"</ratings>";
+	
 	@Test
 	public void testRatingSetFromXml() throws Exception
 	{
 			RatingSet.fromXml(_xmlText);
+			RatingSet ratingSet = RatingSet.fromXml(_xmlText1);
+			AbstractRating[] absRatings = ratingSet.getRatings();
+			
+			int iCount = absRatings[0].getIndParamCount();
+			RatingValue[] tRating = ((TableRating)absRatings[0]).values;
+			// this should not be null because we have 4 ind parameters. There should be a dep Table
+			assertNotNull(tRating[0].depTable);
+			return;
 	}
 
 }
