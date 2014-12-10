99d5be6fceefe7eea2788c6ce36955211a81464e
Pete Morris <psmorris@rmanet.com>
2014-12-10 15:21:55 -0800

    more tests.
    [git-p4: depot-paths = "//hecjavadev/dev/test/hec/data/cwmsRating/": change = 62860]

--- TestUsgsStreamTableRating.java
+++ TestUsgsStreamTableRating.java
@@ -10,11 +10,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.TreeMap;
-import java.util.Vector;
 import java.util.logging.Logger;
-
-import javax.swing.JComboBox;
 
 import junit.framework.Assert;
 
@@ -60,6 +56,7 @@ public class TestUsgsStreamTableRating
 			Assert.assertEquals("Value is out of range low.", e.getMessage());
 		}
 	}
+
 	@Test
 	public final void testStreamRatingGreaterThanMaxExtent() throws Exception
 	{
@@ -84,7 +81,7 @@ public class TestUsgsStreamTableRating
 		Logger logger = Logger.getLogger(TestUsgsStreamTableRating.class.getName());
 		AbstractRating rating = ratings[0];
 		double[][] ratingExtents = rating.getRatingExtents(time);
-		double[] ratingExtent = ratingExtents[ratingExtents.length-1];
+		double[] ratingExtent = ratingExtents[ratingExtents.length - 1];
 		logger.info(ratingExtent[0] + "  " + ratingExtent[1]);
 		logger.info("rating at: " + (ratingExtent[0] + 10.0));
 		try
@@ -125,7 +122,7 @@ public class TestUsgsStreamTableRating
 		double[] ratingExtent = ratingExtents[0];
 		logger.info(ratingExtent[0] + "  " + ratingExtent[1]);
 		double rateOne = testRatingSet.rateOne(time, ratingExtent[0]);
-		Assert.assertEquals(ratingExtent[1], rateOne,0.001);
+		Assert.assertEquals(ratingExtent[1], rateOne, 0.001);
 	}
 
 	@Test
@@ -155,7 +152,7 @@ public class TestUsgsStreamTableRating
 		double[] ratingExtent = ratingExtents[ratingExtents.length - 1];
 		logger.info(ratingExtent[0] + "  " + ratingExtent[1]);
 		double rateOne = testRatingSet.rateOne(time, ratingExtent[0]);
-		Assert.assertEquals(ratingExtent[1], rateOne,0.001);
+		Assert.assertEquals(ratingExtent[1], rateOne, 0.001);
 	}
 
 	@Test
@@ -190,12 +187,8 @@ public class TestUsgsStreamTableRating
 		double rateOne = testRatingSet.rateOne(time, midIndep);
 		UsgsStreamTableRating streamTableRating = (UsgsStreamTableRating) rating;
 		RatingSet shifts = streamTableRating.getShifts();
-		TreeMap<Long, AbstractRating> ratingsMap = shifts.getRatingsMap();
-		Vector<Date> dates = new Vector<Date>();
-		JComboBox cb = new JComboBox<>(dates);
 		
-		
-		
+
 	}
 
 	@Test
@@ -216,33 +209,130 @@ public class TestUsgsStreamTableRating
 		RatingSet testRatingSet = RatingSet.fromXml(sb.toString());
 		AbstractRating[] ratings = testRatingSet.getRatings();
 		AbstractRating rating = ratings[0];
-			UsgsStreamTableRating streamTableRating = (UsgsStreamTableRating) rating;
+		UsgsStreamTableRating streamTableRating = (UsgsStreamTableRating) rating;
 		RatingSet shifts = streamTableRating.getShifts();
 		Assert.assertNull(shifts);
 		Calendar calendar = Calendar.getInstance();
 		calendar.clear();
-        calendar.set(2014,9,1);
-        Date shiftDate = calendar.getTime();
-		
-        List<RatingValueContainer> stageShiftValues = new ArrayList<>();
-        RatingValueContainer rvc = new RatingValueContainer();
-        rvc.indValue = 4.7244;
-        rvc.depValue = -0.012192;
-        stageShiftValues.add(rvc);
-        rvc = new RatingValueContainer();
-        rvc.indValue = 5.30352;
-        rvc.depValue = 0.0;
-        stageShiftValues.add(rvc);
-        
-		boolean shiftActive = true;		
+		calendar.set(2014, 9, 1);
+		Date shiftDate = calendar.getTime();
+
+		List<RatingValueContainer> stageShiftValues = new ArrayList<>();
+		RatingValueContainer rvc = new RatingValueContainer();
+		rvc.indValue = 15.0;
+		rvc.depValue = -1.0;
+		stageShiftValues.add(rvc);
+		rvc = new RatingValueContainer();
+		rvc.indValue = 17.0;
+		rvc.depValue = 1.0;
+		stageShiftValues.add(rvc);
+
+		boolean shiftActive = true;
 		TableRating newShift = streamTableRating.addShift(shiftDate, stageShiftValues, shiftActive);
 		Assert.assertNotNull(newShift);
-		
-		calendar.add(Calendar.MONTH,1);
+
+		calendar.add(Calendar.MONTH, 1);
 		shiftDate = calendar.getTime();
-		
+
+		shiftActive = false;
 		TableRating newShift2 = streamTableRating.addShift(shiftDate, stageShiftValues, shiftActive);
 		Assert.assertNotNull(newShift2);
-			
+		
+		RatingSet shiftsRatingSet = streamTableRating.getShifts();
+		Assert.assertTrue(!shiftsRatingSet.activeRatings.isEmpty());
+		
+		double rateOne = streamTableRating.rateOne(System.currentTimeMillis(),16.0);
+		
 	}
+
+	@Test
+	public final void testRateOneManyShifts() throws Exception
+	{
+		Logger logger = Logger.getLogger(TestUsgsStreamTableRating.class.getName());
+
+		// Retrieve complete RatingSet from local xml file
+		String path = "C:/temp/92F_Stage_Flow_BASE_PRODUCTION-2012-manyshifts.xml";
+		File f = new File(path);
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String line;
+		StringBuilder sb = new StringBuilder();
+		while ((line = br.readLine()) != null)
+		{
+			sb.append(line.trim());
+		}
+		RatingSet testRatingSet = RatingSet.fromXml(sb.toString());
+		AbstractRating[] ratings = testRatingSet.getRatings();
+		AbstractRating rating = ratings[0];
+		UsgsStreamTableRating streamTableRating = (UsgsStreamTableRating) rating;
+		long time = 1415230649194L;//System.currentTimeMillis();
+		double[][] ratingExtents = rating.getRatingExtents(time);
+		double[] ratingExtent = ratingExtents[0];
+		logger.info(ratingExtent[0] + "  " + ratingExtent[1]);
+		double rateOne = testRatingSet.rateOne(time, ratingExtent[0]);
+		logger.info(streamTableRating.toXmlString("  ", 1));
+		
+		//add a shift and rate again.
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear();
+		calendar.set(2014, 11, 1);
+		Date shiftDate = calendar.getTime();
+
+		List<RatingValueContainer> stageShiftValues = new ArrayList<>();
+		RatingValueContainer rvc = new RatingValueContainer();
+		rvc.indValue = 15.0;
+		rvc.depValue = -1.0;
+		stageShiftValues.add(rvc);
+		rvc = new RatingValueContainer();
+		rvc.indValue = 17.0;
+		rvc.depValue = 1.0;
+		stageShiftValues.add(rvc);
+
+		boolean shiftActive = true;
+		TableRating newShift = streamTableRating.addShift(shiftDate, stageShiftValues, shiftActive);
+		
+		ratingExtents = rating.getRatingExtents(shiftDate.getTime());
+		ratingExtent = ratingExtents[0];
+		logger.info(ratingExtent[0] + "  " + ratingExtents[1][0]);
+		rateOne = testRatingSet.rateOne(time, ((ratingExtent[0] + ratingExtents[1][0]) / 2));
+		logger.info(streamTableRating.toXmlString("  ", 1));
+	}
+
+	@Test
+	public final void testStreamRatingNonMonotonicallyIncreasing() throws Exception
+	{
+		// Retrieve complete RatingSet from local xml file
+		String path = "C:/temp/92F_Stage_Flow_BASE_PRODUCTION-2012-not-monotonically-increasing.xml";
+		File f = new File(path);
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String line;
+		StringBuilder sb = new StringBuilder();
+		while ((line = br.readLine()) != null)
+		{
+			sb.append(line.trim());
+		}
+		RatingSet testRatingSet = RatingSet.fromXml(sb.toString());
+		AbstractRating[] ratings = testRatingSet.getRatings();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		long time = cal.getTimeInMillis();
+		Logger logger = Logger.getLogger(TestUsgsStreamTableRating.class.getName());
+		AbstractRating rating = ratings[0];
+		double[][] ratingExtents = rating.getRatingExtents(time);
+		double[] ratingExtent = ratingExtents[ratingExtents.length - 1];
+		logger.info(ratingExtent[0] + "  " + ratingExtent[1]);
+		logger.info("rating at: " + (ratingExtent[0] + 10.0));
+		try
+		{
+			double rateOne = testRatingSet.rateOne(time, ratingExtent[0] + 10.0);
+			Assert.fail("should have failed with out of range high.");
+		}
+		catch (RatingException e)
+		{
+			Assert.assertEquals("Value is out of range high.", e.getMessage());
+		}
+	}
+
 }
