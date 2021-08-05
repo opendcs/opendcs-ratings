6dbbb4de1f7bffc430394cbe9ec43c1157041ea6
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-08-05 08:48:40 -0700

    add read coverage task.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -76,6 +76,13 @@ object Build : BuildType({
             tasks = ":test"
             name = "test Project"
         }
+        script {
+            name = "read in coverage"
+            scriptContent = """
+                # the *Test.* is there in addition to *Test as the teamcity reading is also reading in the callbacks that were made for the unit tests.
+                echo "##teamcity[jacocoReport dataPath='build/jacoco/test.exec']"
+            """.trimIndent()
+        }
         gradle {
             tasks = ":sonarqube"
             name = "SonarQube Analysis"
