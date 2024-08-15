3da440cab0212db21572efa8c08de64ace2e0d17
Adam Korynta <adam@rmanet.com>
2024-08-15 10:50:19 -0700

    add integration test for lazy usgs stream rating classcastexception

--- hec-cwms-ratings-io-jdbc/src/test/java/mil/army/usace/hec/cwms/rating/io/jdbc/JdbcTableRatingTest.java
+++ hec-cwms-ratings-io-jdbc/src/test/java/mil/army/usace/hec/cwms/rating/io/jdbc/JdbcTableRatingTest.java
@@ -18,6 +18,7 @@ import hec.data.cwmsRating.RatingSet;
 import hec.data.cwmsRating.RatingValue;
 import hec.data.cwmsRating.TableRating;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -26,28 +27,47 @@ import java.util.Date;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import mil.army.usace.hec.cwms.rating.io.xml.RatingXmlFactory;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import usace.cwms.db.jooq.codegen.packages.CWMS_ENV_PACKAGE;
+
 final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
 
-    private AbstractRatingSet ratingSet;
+    private AbstractRatingSet tableRatingSet;
+    private AbstractRatingSet usgsTableRatingSet;
 
     @BeforeEach
     public void setup() throws Exception {
-        try (InputStream inputStream = getClass().getResourceAsStream("table_rating.xml");
-             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-             Stream<String> stream = bufferedReader.lines()) {
-            String text = stream.collect(Collectors.joining("\n"));
-            ratingSet = RatingXmlFactory.ratingSet(text);
-            getInstance().connection(conn -> {
-                try {
+        String tableRating = "table_rating.xml";
+        tableRatingSet = storeRating(tableRating);
+        String usgsTableRating = "usgs_stream_table_rating.xml";
+        usgsTableRatingSet = storeRating(usgsTableRating);
+    }
+
+    private AbstractRatingSet storeRating(String usgsTableRating) throws IOException, RatingException, SQLException
+    {
+        try(InputStream inputStream = getClass().getResourceAsStream(usgsTableRating);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            Stream<String> stream = bufferedReader.lines())
+        {
+            String text = stream.collect(Collectors.joining("\n")).replace("\"NAB\"", "\"" + getInstance().getOfficeId() + "\"");
+            AbstractRatingSet ratingSet = RatingXmlFactory.ratingSet(text);
+            getInstance().connection(conn ->
+            {
+                try
+                {
+                    CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                     RatingJdbcFactory.store(ratingSet, conn, true, true);
-                } catch (RatingException e) {
+                }
+                catch(RatingException e)
+                {
                     throw new RuntimeException(e);
                 }
             });
+            return ratingSet;
         }
     }
 
@@ -55,10 +75,11 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testEagerRatingSetRetrieval() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 AbstractRatingSet abstractRatingSet = RatingJdbcFactory.ratingSet(RatingSet.DatabaseLoadMethod.EAGER, new TransientConnectionProvider(conn),
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertEquals(ratingSet, abstractRatingSet);
-                assertEquals(1, ratingSet.getRatings().length);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertEquals(tableRatingSet, abstractRatingSet);
+                assertEquals(1, tableRatingSet.getRatings().length);
                 assertEquals(12.0, abstractRatingSet.rate(new Date().getTime(), 392.0), 0.0);
                 assertEquals(392.0, abstractRatingSet.reverseRate(new Date().getTime(), 12.0), 0.0);
             } catch (RatingException e) {
@@ -71,10 +92,11 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testEagerRatingSetRetrievalBackwardsCompatible() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 RatingSet abstractRatingSet = RatingSet.fromDatabase(RatingSet.DatabaseLoadMethod.EAGER, conn,
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertEquals(ratingSet, abstractRatingSet);
-                assertEquals(1, ratingSet.getRatings().length);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertEquals(tableRatingSet, abstractRatingSet);
+                assertEquals(1, tableRatingSet.getRatings().length);
                 assertEquals(12.0, abstractRatingSet.rate(new Date().getTime(), 392.0), 0.0);
                 assertEquals(392.0, abstractRatingSet.reverseRate(new Date().getTime(), 12.0), 0.0);
             } catch (RatingException e) {
@@ -87,9 +109,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testLazyRatingSetRetrievalGetConcreteRatings() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 AbstractRatingSet abstractRatingSet = RatingJdbcFactory.ratingSet(RatingSet.DatabaseLoadMethod.LAZY, new TransientConnectionProvider(conn),
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof LazyJdbcRatingSet);
                 assertEquals(1, abstractRatingSet.getRatings().length);
                 TableRating rating = (TableRating) abstractRatingSet.getRatings()[0];
@@ -111,9 +134,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testLazyRatingSetRetrievalGetConcreteRatingsBackwardsCompatible() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 RatingSet abstractRatingSet = RatingSet.fromDatabase(RatingSet.DatabaseLoadMethod.LAZY, conn,
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof LazyJdbcRatingSet);
                 assertEquals(1, abstractRatingSet.getRatings().length);
                 TableRating rating = (TableRating) abstractRatingSet.getRatings()[0];
@@ -135,9 +159,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testLazyRatingSetRetrieval() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 AbstractRatingSet abstractRatingSet = RatingJdbcFactory.ratingSet(RatingSet.DatabaseLoadMethod.LAZY, new TransientConnectionProvider(conn),
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof LazyJdbcRatingSet);
                 assertEquals(12.0, abstractRatingSet.rate(conn, new Date().getTime(), 392.0), 0.0);
                 assertEquals(392.0, abstractRatingSet.reverseRate(conn, new Date().getTime(), 12.0), 0.0);
@@ -151,9 +176,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testLazyRatingSetRetrievalBackwardsCompatible() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 RatingSet abstractRatingSet = RatingSet.fromDatabase(RatingSet.DatabaseLoadMethod.LAZY, conn,
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof LazyJdbcRatingSet);
                 assertEquals(12.0, abstractRatingSet.rate(conn, new Date().getTime(), 392.0), 0.0);
                 assertEquals(392.0, abstractRatingSet.reverseRate(conn, new Date().getTime(), 12.0), 0.0);
@@ -167,9 +193,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testReferenceRatingSetRetrieval() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 AbstractRatingSet abstractRatingSet = RatingJdbcFactory.ratingSet(RatingSet.DatabaseLoadMethod.REFERENCE, new TransientConnectionProvider(conn),
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof ReferenceJdbcRatingSet);
                 assertEquals(0, abstractRatingSet.getRatings().length);
                 abstractRatingSet.setDataUnits(conn, new String[]{"ft", "acre"});
@@ -185,9 +212,10 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
     public void testReferenceRatingSetRetrievalBackwardsCompatible() throws SQLException {
         getInstance().connection(conn -> {
             try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
                 RatingSet abstractRatingSet = RatingSet.fromDatabase(RatingSet.DatabaseLoadMethod.REFERENCE, conn,
-                    getInstance().getOfficeId(), ratingSet.getRatingSpec().getRatingSpecId(), null, null, true);
-                assertNotEquals(ratingSet, abstractRatingSet);
+                    getInstance().getOfficeId(), tableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(tableRatingSet, abstractRatingSet);
                 assertTrue(abstractRatingSet instanceof ReferenceJdbcRatingSet);
                 assertEquals(0, abstractRatingSet.getRatings().length);
 
@@ -200,5 +228,20 @@ final class JdbcTableRatingTest extends CwmsDockerIntegrationTest {
         });
     }
 
-
+    @Test
+    public void testLazyRatingSetReverseRate() throws SQLException {
+        getInstance().connection(conn -> {
+            try {
+                CWMS_ENV_PACKAGE.call_SET_SESSION_OFFICE_ID(DSL.using(conn).configuration(), getInstance().getOfficeId());
+                RatingSet abstractRatingSet = RatingSet.fromDatabase(RatingSet.DatabaseLoadMethod.LAZY, conn,
+                        getInstance().getOfficeId(), usgsTableRatingSet.getRatingSpec().getRatingSpecId(), null, null, true);
+                assertNotEquals(usgsTableRatingSet, abstractRatingSet);
+                assertTrue(abstractRatingSet instanceof LazyJdbcRatingSet);
+                assertEquals(2.9, abstractRatingSet.rate(conn, new Date().getTime(), 1.5), 0.1);
+                assertEquals(1.5, abstractRatingSet.reverseRate(conn, new Date().getTime(), 2.9), 0.1);
+            } catch (RatingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
