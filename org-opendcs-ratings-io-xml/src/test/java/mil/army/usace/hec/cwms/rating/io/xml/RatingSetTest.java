2b9f1b7f03e9c92048a7156ec2a0a87b2f917c96
Karl Tarbet <ktarbet@users.noreply.github.com>
2026-02-02 08:01:52 -0800

    update java file headers

--- org-opendcs-ratings-io-xml/src/test/java/mil/army/usace/hec/cwms/rating/io/xml/RatingSetTest.java
+++ org-opendcs-ratings-io-xml/src/test/java/mil/army/usace/hec/cwms/rating/io/xml/RatingSetTest.java
@@ -1,9 +1,18 @@
 /*
- * Copyright (c) 2022
- * United States Army Corps of Engineers - Hydrologic Engineering Center (USACE/HEC)
- * All Rights Reserved.  USACE PROPRIETARY/CONFIDENTIAL.
- * Source may not be released without written approval from HEC
- */
+* Where Applicable, Copyright 2026 OpenDCS Consortium and/or its contributors
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations 
+* under the License.
+*/
 package org.opendcs.ratings.io.xml;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
